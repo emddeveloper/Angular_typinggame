9c80e8bc3d55c0858945acb103186d32a80fe364
emddeveloper <emd.developer@gmail.com>
2021-03-17 17:07:51 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  sampleText = lorem.sentence();
+  enteredText = '';
+  getInput(value: string) {
+    this.enteredText = value;
+  }
+  compare(letter: string, eletter: string) {
+    if (!eletter) {
+      return 'has-text-black';
+    }
+    return letter === eletter ? 'has-text-primary' : 'has-text-danger';
+  }
 }
